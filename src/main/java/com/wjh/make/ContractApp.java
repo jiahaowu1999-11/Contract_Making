871afce4e0fbcd252c4ed576e495f3433ab1c719
jiahao.wu <zoomkills@outlook.com>
2025-06-21 23:55:02 +0800

    111

--- src/main/java/com/wjh/make/ContractApp.java
+++ src/main/java/com/wjh/make/ContractApp.java
@@ -3,6 +3,7 @@ package com.wjh.make;
 public class ContractApp {
     public static void main(String[] args) {
         System.out.println("1111111111111");
+        System.out.println("1232");
 
     }
 }
